b67ea72d3752f1940f57baf33c5579a4a18ad040
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2022-12-21 23:09:06 -0500

    Update brew-install-script.sh

--- brew-install-script.sh
+++ brew-install-script.sh
@@ -1,13 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash 
+echo hello world 
 
-if brew --version > /dev/null; then 
-  echo "brew is already installed"
-else 
-    echo "Installing brew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    
-if python3 --version > /dev/null; then 
-  echo "python3 is already installed"
-else 
-    echo "Installing python3..."
-    brew install python
