2309ef0d9bb7d90c7899e7addb831e194e92951d
Team Tinitiate / Venkata Bhattaram <tinitiate@users.noreply.github.com>
2022-12-29 20:42:30 -0500

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,7 +1,7 @@
 $check=((gp HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName -Match "chocolatey").Length -gt 0
 if(-not($checkchoco)){
     Write-Output "Seems Chocolatey is not installed, installing now"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 else{
     Write-Output "Chocolatey Version $checkchoco is already installed"
