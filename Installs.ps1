e31659f34cd11e565b7e9a824e417e3efb05b5ea
Team Tinitiate / Venkata Bhattaram <tinitiate@users.noreply.github.com>
2022-12-29 20:20:55 -0500

    Update Installs.ps1

--- Installs.ps1
+++ Installs.ps1
@@ -1,7 +1,7 @@
 ﻿$checkchoco = powershell choco -v
 if(-not($checkchoco)){
     Write-Output "Seems Chocolatey is not installed, installing now"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    powershell Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 else{
     Write-Output "Chocolatey Version $checkchoco is already installed"
